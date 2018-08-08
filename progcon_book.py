5d0267f648e7fe8f9a7ef1c7a5a5d7d14a95cf39
ababa893 <flvonlineconverter@gmail.com>
2018-08-09 00:48:19 +0900

    Update progcon_book

--- progcon_book.py
+++ progcon_book.py
@@ -58,3 +58,63 @@ for val in input_list:
 print(stack[0])
 
 # 4.3 ALDS1_3_B Queue
+# Wrong answer at sight (Conditional branch should be modified)
+n_process, quantum = map(int, input().split())
+p_t_list = [input().split() for _ in range(n_process)]
+
+fincount = 0
+i = 0
+elapsed_time = 0
+while(fincount < n_process):
+    if i == n_process:
+        i = 0
+    p = p_t_list[i][0]
+    t = int(p_t_list[i][1])
+    if t <= 0:
+        print(p, elapsed_time)
+        fincount += 1
+    else:
+        p_t_list[i][1] = str(t - quantum)
+        elapsed_time = elapsed_time + min(t, quantum)
+    i += 1
+# Second trial
+import numpy as np
+import queue
+p = queue.Queue()
+t = queue.Queue()
+
+n_process, quantum = map(int, input().split())
+p_t_list = np.array([input().split() for _ in range(n_process)])
+
+for p_i, t_i in zip(p_t_list[:, 0], p_t_list[:, 1]):
+    p.put(p_i)
+    t.put(int(t_i))
+
+fincount = 0
+elapsed_time = 0
+finish_p = []
+while(fincount < n_process):
+    # Get process_name and time
+    p_val = p.get()
+    t_val = t.get()
+    # Calculate processing time
+    elapsed_time = elapsed_time + min(t_val, quantum)
+    # Update remaining time
+    t_val = max(t_val - quantum, 0)
+    # Check whether the process is finished
+    if t_val == 0 and p_val not in finish_p:
+        print(p_val, elapsed_time)
+        fincount += 1
+        finish_p.append(p_val)
+    # Add process to end of the queue
+    p.put(p_val)
+    t.put(t_val)
+    
+
+
+
+
+
+
+
+
