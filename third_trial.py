1652fb931fe9fc15534314103463032b75ec4fd1
ababa893 <flvonlineconverter@gmail.com>
2018-08-16 23:24:23 +0900

    Update third_trial

--- third_trial.py
+++ third_trial.py
@@ -28,3 +28,15 @@ while(is_even):
         n_operation += 1
 
 print(n_operation)
+
+#4.
+a, b, c, x = map(int, [input() for _ in range(4)])
+
+n_selection = 0
+for i in range(a+1):
+    for j in range(b+1):
+        for k in range(c+1):
+            if 500 * i + 100 * j + 50 * k == x:
+                n_selection += 1
+
+print(n_selection)
