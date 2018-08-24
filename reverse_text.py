159f7849def8c875720d324a0a4b6a99fa5f2c45
ababa893 <flvonlineconverter@gmail.com>
2018-08-24 17:57:12 +0900

    Add an answer of string reversing problem

--- reverse_text.py
+++ reverse_text.py
@@ -1,5 +1,5 @@
 # The code is not related to ProgContest problem which were introduced in README.md
-# I just try a string reversing problems (http://japanese.joelonsoftware.com/Articles/Interviewing.html) in Python
+# I just try a string reversing problem (http://japanese.joelonsoftware.com/Articles/Interviewing.html) in Python
 str_list = list(input())
 str_len = len(str_list)
 for i in range(int(len(str_list) / 2)):
