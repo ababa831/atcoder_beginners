2313ea8a8544410ecc72b088f9c7cdb487913cff
ababa893 <flvonlineconverter@gmail.com>
2018-08-28 04:11:37 +0900

    Update third_trial

--- third_trial.py
+++ third_trial.py
@@ -110,3 +110,13 @@ if s == "":
     print("YES")
 else:
     print("NO")
+
+#10. OK
+n = int(input())
+for i in range(n):
+    t, x, y = map(int, input().split())
+    if t < x + y or t % 2 != (x + y) % 2:
+        print("No")
+        exit()
+        
+print("Yes")
