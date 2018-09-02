746a8b9745df148ce29c64d1dfa4c39fbe229a44
ababa893 <flvonlineconverter@gmail.com>
2018-09-03 00:51:49 +0900

    Add b_grid_compression_2nd trial

---
+++ contest_107/b_grid_compression_2nd.py
@@ -0,0 +1,21 @@
+h, w = map(int, input().split())
+grid = [list(input()) for _ in range(h)]
+
+# Filtering the data if any "#" are existed in the each row or col
+"""
+Usage: 
+filter(lambda row: <filtering condition>, grid or zip(*grid)) 
+If true, the row is not be deleted
+(Do not use if statement!)
+"""
+row_compressed_grid = list(filter(lambda row: any(x == "#" for x in row), grid))
+compressed_grid = list(filter(lambda col: any(x == "#" for x in col), zip(*row_compressed_grid)))
+
+# list -> string and out
+"""
+Note: 
+when filtered by zip(*grid), the string order of the grid was transposed.
+So the compressed grid should be zip(*compressed_grid) again.
+"""
+for row in zip(*compressed_grid):
+    print("".join(row))
