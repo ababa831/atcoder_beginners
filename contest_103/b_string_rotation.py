b6ef285f45c94358b77a19f0fa2c8712eec84031
ababa893 <flvonlineconverter@gmail.com>
2018-09-04 02:39:37 +0900

    Add contest 103

---
+++ contest_103/b_string_rotation.py
@@ -0,0 +1,15 @@
+# Accepted
+s_list = list(input())
+t_list = list(input())
+n_oneroop = len(s_list)
+
+for _ in range(n_oneroop):
+    s_list = [s_list[-1]] + s_list[:-1]
+    if s_list == t_list:
+        print("Yes")
+        exit()
+print("No")
+
+# A sample answer written in one liner
+# s = input()
+# print('Yes' if input() in s + s else 'No')
