a52eabf27278190f515b33c7cbd2270e328e96d9
ababa893 <flvonlineconverter@gmail.com>
2018-09-04 03:28:54 +0900

    Add contest 104

---
+++ contest_104/b_accepted.py
@@ -0,0 +1,25 @@
+"""
+Accepted, but awkward thinking and I could not recall how to:
+- count specific characters such as "A", "C"
+- filter strings which do not have upper letter without "A", "C"
+
+The solutions were searched.
+"""
+s = list(input())
+
+if "A" != s[0]:
+    print("WA")
+    exit()
+s_extracted = s[2:-1]
+if s_extracted.count("C") != 1:
+    print("WA")
+    exit()
+# Get Upper characters without "A", "C"
+s_upper = [
+    chara for chara in s if chara != "C" and chara != "A" and chara.isupper()
+]
+if len(s_upper) > 0:
+    print("WA")
+    exit()
+
+print("AC")
