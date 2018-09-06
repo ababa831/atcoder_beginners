56e38fb44a226a7f7bf1fbd0fd89d47a6ef6f3f6
ababa893 <flvonlineconverter@gmail.com>
2018-09-06 15:38:04 +0900

    update b_maximum_difference

--- contest_102/b_maximum_difference.py
+++ contest_102/b_maximum_difference.py
@@ -1,6 +1,6 @@
 # Accepted
 # (I think a test giver expect you should solve the probrem by full scratch, 
-# not using map() or min() functions.)
+# not using max() or min() functions.)
 n = int(input())
 a_list = list(map(int, input().split()))
 print(max(a_list) - min(a_list))
