8e8b3392436e7e800af415dd7cf993f2e912a253
ababa893 <flvonlineconverter@gmail.com>
2018-09-08 00:25:41 +0900

    Update contest 100

--- contest_100/b_ringos_favorite_numbers.py
+++ contest_100/b_ringos_favorite_numbers.py
@@ -1,2 +1,16 @@
-n, d = map(int, input().split())
-print((100**n)*d)
+# Wrong Answer:
+# Some cases were wrong, but I could not detect wrong ones.
+d, n = map(int, input().split())
+print((100**d) * n)
+
+# Consider the case: n = 100
+# If n = 100, (100 ** d) * n -> 100 ** (d + 1) is correct
+# So, in the situation, 
+# you must change the calculation method: (100 ** d) * 101
+"""
+d, n = map(int, input().split())
+if n == 100:    
+    print((100**d) * (n+1))
+else:
+    print((100**d) * n)
+"""
