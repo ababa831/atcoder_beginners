003b917006dae90442c8f11cdf03c765b1636535
ababa893 <flvonlineconverter@gmail.com>
2018-09-14 05:01:38 +0900

    Update contest 095

--- contest_095/b_bitter_alchemy.py
+++ contest_095/b_bitter_alchemy.py
@@ -4,6 +4,6 @@ m_list = list(map(int, [input() for _ in range(n)]))
 
 m_sum = sum(m_list)
 x_residue = x - m_sum
-n_add_donuts = int(x_residue / min(m_list))
+n_additional = int(x_residue / min(m_list))
 
-print(n + n_add_donuts)
+print(n + n_additional)
