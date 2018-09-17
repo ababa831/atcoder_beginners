0b019c5eb7aa1f2925e97fa134ae2f69e7be70c6
ababa893 <flvonlineconverter@gmail.com>
2018-09-17 22:13:58 +0900

    Update b_small_and_large_integers

--- contest_093/b_small_and_large_integers.py
+++ contest_093/b_small_and_large_integers.py
@@ -20,4 +20,20 @@ for out_val in out_list:
     if a <= out_val <= b:
         print(out_val)
 """
-
+# TODO: Examine which codes is faster.
+# 1. Using list comprehension
+# (I want to know if the sum of the two lists
+# as shown in below takes computation time.)
+"""
+lower_list = [i for i in range(a, a + k)]
+upper_list = [i for i in range(b, b - k, -1)]
+out_list = sorted(set(lower_list + upper_list))
+"""
+# 2. append values to empty list
+"""
+out_list = []
+for i in range(k):
+    out_list.append(a + i)
+    out_list.append(b - (k - 1 - i))
+out_list = sorted(set(out_list))
+"""
