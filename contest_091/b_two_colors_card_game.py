84cf5bef7598ff04f1c1fe3f82f20da785019c59
ababa893 <flvonlineconverter@gmail.com>
2018-09-21 18:48:28 +0900

    Add contest_091

---
+++ contest_091/b_two_colors_card_game.py
@@ -0,0 +1,13 @@
+# Accepted
+n = int(input())
+s_list = [input() for _ in range(n)]
+m = int(input())
+t_list = [input() for _ in range(m)]
+
+max_profit = -100  # M <= 100 -> max_profit is larger than -100
+canditates = set(s_list)
+for canditate in canditates:
+    tmp_profit = s_list.count(canditate) - t_list.count(canditate)
+    if tmp_profit > max_profit:
+        max_profit = tmp_profit   
+print(max(max_profit, 0))  # out_put > 0
