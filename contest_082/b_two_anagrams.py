4613a85c98bfeb52fde5bd3f2b4c77f5eb12c4cb
ababa893 <flvonlineconverter@gmail.com>
2018-10-11 00:05:48 +0900

    Add contest_082

--- contest_082/b_two_anagrams.py
+++ contest_082/b_two_anagrams.py
@@ -1,20 +1,23 @@
+# Accepted, but too long codes.
 s = sorted(input())
 t = sorted(input(), reverse=True)
 len_s = len(s)
 len_t = len(t)
 n_passed = 0
 
-for i in range(len_s):
+min_len = min(len_s, len_t)
+for i in range(min_len):
     if s[i] < t[i]:
         print("Yes")
         exit()
 
-t = sorted(t)
 if len_s < len_t:
+    t = sorted(t)
     for i in range(len_s):
         if s[i] == t[i]:
             n_passed += 1
     if n_passed == len_s:
         print("Yes")
         exit()
+
 print("No")
