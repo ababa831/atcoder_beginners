ea71cb140b5e0f12a148f9a54e92591d5dd87699
ababa893 <flvonlineconverter@gmail.com>
2018-10-12 20:08:54 +0900

    Update contest_081

--- contest_081/b_shift_only.py
+++ contest_081/b_shift_only.py
@@ -4,7 +4,7 @@ a_list = list(map(int, input().split()))
 
 n_division = 0
 while True:
-    a_list = [a/2 for a in a_list if a % 2 == 0]
+    a_list = [a / 2 for a in a_list if a % 2 == 0]
     if len(a_list) != n:
         print(n_division)
         exit()
