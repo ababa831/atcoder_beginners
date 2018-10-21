788fa4cebe6273a8e4f5e96e11aeb230984b4130
ababa893 <flvonlineconverter@gmail.com>
2018-10-22 08:42:22 +0900

    Add contest_075

--- contest_075/b_minesweeper.py
+++ contest_075/b_minesweeper.py
@@ -1,30 +1,20 @@
-# In progress
+# Accepted
 h, w = map(int, input().split())
 # Setting
 mine_set = set()
+next_to_grid_list = [(1, 0), (1, -1), (0, -1), (-1, -1), 
+                     (-1, 0), (-1, 1), (0, 1), (1, 1)]
 
 
 def add_number_for_dot(mine):
-    if is_constrained(mine[0] + 1, mine[1] + 0]) and type(grids[mine[0] + 1][mine[1] + 0]) is int:
-        grids[mine[0] + 1][mine[1] + 0] += 1
-    if type(grids[mine[0] + 1][mine[1] - 1]) is int:
-        grids[mine[0] + 1][mine[1] - 1] += 1
-    if type(grids[mine[0] + 0][mine[1] - 1]) is int:
-        grids[mine[0] + 0][mine[1] - 1] += 1
-    if type(grids[mine[0] - 1][mine[1] - 1]) is int:
-        grids[mine[0] - 1][mine[1] - 1] += 1
-    if type(grids[mine[0] - 1][mine[1] + 0]) is int:
-        grids[mine[0] - 1][mine[1] + 0] += 1
-    if type(grids[mine[0] - 1][mine[1] + 1]) is int:
-        grids[mine[0] - 1][mine[1] + 1] += 1
-    if type(grids[mine[0] + 0][mine[1] + 1]) is int:
-        grids[mine[0] + 0][mine[1] + 1] += 1
-    if type(grids[mine[0] + 1][mine[1] + 1]) is int:
-        grids[mine[0] + 1][mine[1] + 1] += 1
+    for next_to_grid in next_to_grid_list:
+        if is_constrained(mine[0] + next_to_grid[0], mine[1] + next_to_grid[1]):
+            if type(grids[mine[0] + next_to_grid[0]][mine[1] + next_to_grid[1]]) is int:
+                grids[mine[0] + next_to_grid[0]][mine[1] + next_to_grid[1]] += 1
 
 
 def is_constrained(i, j):
-    if 0 <= i < h or 0 <= j < w:
+    if 0 <= i < h and 0 <= j < w:
         return True
     else:
         return False
@@ -44,6 +34,7 @@ for i in range(h):
 for mine in mine_set:
     add_number_for_dot(mine)
 
+# Show result
 for i in range(h):
     s_i = [str(grid) if type(grid) is int else grid for grid in grids[i]]
     print("".join(s_i))
