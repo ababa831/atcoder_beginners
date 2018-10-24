0092ec65c037c86702a9d84860b7577ff267d138
ababa893 <flvonlineconverter@gmail.com>
2018-10-24 20:55:54 +0900

    add alds1_1_d_2nd.py

---
+++ progcon_book/alds1_1_d_2nd.py
@@ -0,0 +1,11 @@
+# Accepted
+n = int(input())
+r_list = list(map(int, [input() for _ in range(n)]))
+
+min_val = r_list[0]
+max_diff = -2 * 10**9
+for i in range(1, n):
+    max_diff = max(max_diff, r_list[i] - min_val)
+    min_val = min(min_val, r_list[i])
+
+print(max_diff)
