df75374a7427fc263a967032693f8f2841f8e382
ababa893 <flvonlineconverter@gmail.com>
2018-11-06 01:45:24 +0900

    Add contest_113

--- contest_113/b.py
+++ contest_113/b.py
@@ -3,12 +3,12 @@ n = int(input())
 t, a = map(int, input().split())
 h_list = map(int, input().split())
 
-min_tempdiff = 10000  # 十分大きい値で！
+min_tempdiff = 10000  # Set an enough large number
 argmin_tempdiff = None
 for i, h_val in enumerate(h_list):
     temp_diff = abs(a - (t - h_val * 0.006))
     if min_tempdiff > temp_diff:
         min_tempdiff = temp_diff
-        argmin_tempdiff = i + 1  # 地点は配列インデックス+1
+        argmin_tempdiff = i + 1  # spot index: idx+1
 
 print(argmin_tempdiff)
