bad2a55d23fec045ca193cb1c0e51e5df5c9d0a3
ababa893 <flvonlineconverter@gmail.com>
2018-11-17 18:09:18 +0900

    Update alds1_2_c_2nd

--- progcon_book/alds1_2_c_2nd.py
+++ progcon_book/alds1_2_c_2nd.py
@@ -1,30 +1,44 @@
-# In progress
+# Accepted
 def bubble_sort(a, n):
+    a_b = a.copy()
     for i in range(n):
         for j in range(n - 1, i, -1):
-            if a[j] < a[j - 1]:
-                a[j], a[j - 1] = a[j - 1], a[j]
-    return a
+            # Do not miss [1] on the comparisons
+            if a_b[j][1] < a_b[j - 1][1]:
+                a_b[j], a_b[j - 1] = a_b[j - 1], a_b[j]
+    return a_b
 
 
 def selection_sort(a, n):
+    a_s = a.copy()
     for i in range(n - 1):
-        min_i = min(a[i:])
-        argmin = a.index(min_i)
-        a[i], a[argmin] = a[argmin], a[i]
-    return a
+        tmp_a = [elem[1] for elem in a_s[i + 1:]]
+        min_i = min(tmp_a)
+        argmin = tmp_a.index(min_i)
+        # Do not miss [1] on the comparisons
+        if a_s[i][1] > a_s[argmin + i + 1][1]:
+            # Note: an argmin index is counted from a_s[0] 
+            # -> Correct indexing: "argmin + i + 1"
+            a_s[i], a_s[argmin + i + 1] = a_s[argmin + i + 1], a_s[i]
+    return a_s
 
 
 def main():
     # Input
     n = int(input())
-    a = list(map(int, input().split()))
+    a = list(input().split())
     # Sort
     a_bubbled = bubble_sort(a, n)
     a_selectioned = selection_sort(a, n)
     # Output
-    print(" ".join(map(str, a_bubbled)))
-    print(" ".join(map(str, a_selectioned)))
+    print(" ".join(a_bubbled))
+    print("Stable")  # Bubble sort is always stable
+    print(" ".join(a_selectioned))
+    # Judge if the selection sort is stable
+    if a_selectioned == a_bubbled:
+        print("Stable")
+    else:
+        print("Not stable")
 
 
 if __name__ == '__main__':
