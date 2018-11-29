b0a8d337750ee8045cd09379678fee197673c099
ababa893 <flvonlineconverter@gmail.com>
2018-11-29 19:37:05 +0900

    Update alds1_3_b.py

--- progcon_book/alds1_3_b.py
+++ progcon_book/alds1_3_b.py
@@ -8,6 +8,30 @@ class Queue(object):
     
     def enqueue(self, x):
         self.Q.append(x)
-    
+        if self.tail is not None:
+            self.tail += 1
+        else:
+            self.head = 0
+            self.tail = self.head + 1
+
     def dequeue(self):
-        pass
+        self.Q.pop(self.head)
+        self.head += 1
+
+if __name__ == "__main__":
+    # Input
+    n, q = list(map(int, input().split()))
+    procs = {}
+    for i in range(n):
+        proc_name, proc_time = input().split()
+        proc_time = int(proc_time)
+        procs[proc_name] = proc_time
+    # Processings
+    max_size = int(1000000 / n)
+    queue = Queue(max_size)
+    elapsed_time = 0
+    is_finished = False
+    while is_finished is False:
+        for proc_n, proc_t in procs.items():
+    # TODO: 辞書ごとQueueに登録しないとわからなくなる？
+
