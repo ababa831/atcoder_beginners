7bd2dc6c4fe81e0a59dce9702fa4dd42e11531bb
ababa893 <flvonlineconverter@gmail.com>
2018-11-30 20:30:23 +0900

    Update alds1_3_b.py

--- progcon_book/alds1_3_b.py
+++ progcon_book/alds1_3_b.py
@@ -1,58 +1,49 @@
-# In progress
+# Accepted
 class Queue(object):
     def __init__(self, queue_size):
         self.queue_size = queue_size
-        self.Q = [None for _ in range(self.queue_size)]
+        self.Q = [None] * self.queue_size
         self.head = 0
         self.tail = 0
 
-    def is_empty(self):
-        # An example of fucking codes.
-        """
-        if self.head == self.tail:
-            return True
-        else:
-            return False
-        """
-        return self.head == self.tail
-    
-    def is_full(self):
-        return self.head == (self.tail+1) % self.queue_size
-
     def enqueue(self, x):
-        if self.is_full() is False:
-            self.Q[self.head] == x
-            if self.head + 1 == self.queue_size:
-                self.head = 0
-            else:
-                self.head += 1
-        
+        self.Q[self.tail] = x
+        self.tail = (self.tail + 1) % self.queue_size
+
     def dequeue(self):
-        if self.is_empty() is False:
-            x = self.Q[self.tail]
-            if self.tail + 1 == self.queue_size:
-                self.tail = 0
-            else:
-                self.tail += 1
-            return x
-        else:
-            return None
+        x = self.Q[self.head]
+        self.Q[self.head] = None
+        self.head = (self.head + 1) % self.queue_size
+        return x
 
 
 if __name__ == "__main__":
     # Input
     n, q = list(map(int, input().split()))
-    procs = {}
+    pt = list([input().split() for _ in range(n)])
+    p = [val[0] for val in pt]
+    t = list(map(int, [val[1] for val in pt]))
+    # Enqueue all data
+    q_size = 1000000  # Assumed max_size
+    p_queue = Queue(q_size)
+    t_queue = Queue(q_size)
     for i in range(n):
-        proc_name, proc_time = input().split()
-        proc_time = int(proc_time)
-        procs[proc_name] = proc_time
-    # Processings
-    max_size = int(1000000 / n)
-    queue = Queue(max_size)
+        p_queue.enqueue(p[i])
+        t_queue.enqueue(t[i])
+    # print(t_queue.Q)
+    # Do round-robin
+    n_finished = 0
     elapsed_time = 0
-    is_finished = False
-    while is_finished is False:
-        for proc_n, proc_t in procs.items():
-    # TODO: 辞書ごとQueueに登録しないとわからなくなる？
-
+    while n_finished < n:
+        tmp_t, tmp_p = t_queue.dequeue(), p_queue.dequeue()
+        tmp_lefttime = tmp_t - q
+        if tmp_lefttime <= 0:
+            # 処理が終わった場合
+            elapsed_time += tmp_t
+            print(tmp_p, elapsed_time)
+            n_finished += 1
+        else:
+            # 処理が残っている場合
+            elapsed_time += q
+            t_queue.enqueue(tmp_lefttime)
+            p_queue.enqueue(tmp_p)
