f5fa2d8fec0b47f881f580f78b58a04b842b762f
ababa893 <flvonlineconverter@gmail.com>
2018-12-07 01:42:33 +0900

    Add alds1_3_c.py (In progress)

---
+++ progcon_book/alds1_3_c.py
@@ -0,0 +1,12 @@
+class doublyLinkedList(object):
+    def insert(self, x):
+        pass
+    
+    def delete(self):
+        pass
+
+    def delete_first(self):
+        pass
+    
+    def delete_last(self):
+        pass
