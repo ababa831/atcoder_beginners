c0143a868a8d28f534376a2f968335d1d86b0a1a
ababa893 <flvonlineconverter@gmail.com>
2018-12-08 01:55:29 +0900

    Update alds1_3_c.py (In progress)

--- progcon_book/alds1_3_c.py
+++ progcon_book/alds1_3_c.py
@@ -1,13 +1,29 @@
 # In progress
-class doublyLinkedList(object):
-    def insert(self, x):
-        pass
-    
+# Keypoints of implement doubly linked list
+# Make two roles for the list:
+# - Structure of a node of doubly linked list
+# - Linkage of the nodes
+
+
+class Node(object):
+    def __init__(self, key=None, next=None, prev=None):
+        self.key = key
+        self.next = next
+        self.prev = prev
+
+
+class DoublyLinkedList(object):
+    def insert(self, x, prev_node=None):
+        if prev_node is not None:
+            new_node = Node(key=x, prev=prev_node.next)
+            prev_node.next = new_node.prev
+            
+
     def delete(self):
         pass
 
     def delete_first(self):
         pass
-    
+
     def delete_last(self):
         pass
