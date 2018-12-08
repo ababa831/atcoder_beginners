d42035a31666267bd5031b748dcd4ee1cbfd7327
ababa893 <flvonlineconverter@gmail.com>
2018-12-09 01:27:19 +0900

    Minor fix

--- contest_115/c.py
+++ contest_115/c.py
@@ -3,10 +3,10 @@ n, k = map(int, input().split())
 h_list = list(map(int, [input() for _ in range(n)]))
 h_list = sorted(h_list, reverse=True)
 
-min_diff = 10 ** 9
-for i in range(n-k+1):
+min_diff = 10**9
+for i in range(n - k + 1):
     h_max = h_list[i]
-    h_min = h_list[i+k-1]
+    h_min = h_list[i + k - 1]
     h_diff = h_max - h_min
     if min_diff > h_diff:
         min_diff = h_diff
