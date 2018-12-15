e84fe48cbbb3d33d72aae99411517e0fbe58caf4
ababa893 <flvonlineconverter@gmail.com>
2018-12-16 00:49:49 +0900

    Update agc_029/a.py

--- agc_029/a.py
+++ agc_029/a.py
@@ -1,6 +1,6 @@
 # Accepted
 s = input()
-
+ 
 is_all_swappd = False
 n_swap = 0
 n_white = s.count('W')
@@ -9,8 +9,8 @@ for i in range(n_white):
     if i == 0:
         w_idx = s.find('W')
     else:
-        w_idx = s[last_w_idx + 1:].find('W')
-    n_swap += s[:w_idx].count('B')
+        w_idx = s[last_w_idx + 1:].find('W') + last_w_idx + 1
+    n_swap += w_idx - i
     last_w_idx = w_idx
-    print(n_swap)
+ 
 print(n_swap)
