6006ce984190841d57772c2a8ba6d83191a2e50d
ababa893 <flvonlineconverter@gmail.com>
2019-02-10 09:16:46 -1000

    Add c_3rd

---
+++ minna/c_3rd.py
@@ -0,0 +1,15 @@
+# WA: Someting Wrong
+k, a, b = map(int, input().split())
+
+if b - a <= 1:
+    print(k + 1)
+else:
+    remaining = k - (a - 1)
+    if remaining <= 1:
+        print(k + 1)
+    else:
+        biscket = a + (b - a)**int(remaining / 2)
+        if remaining % 2:
+            # Odd
+            biscket += 1
+        print(biscket)
