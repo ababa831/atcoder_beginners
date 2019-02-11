cf002c88a27f64ae12c9b3413ecc1f40e712d0f4
ababa893 <flvonlineconverter@gmail.com>
2019-02-12 02:14:06 +0900

    add AtCoder 版！蟻本

---
+++ arihon_biginners/arc004.py
@@ -0,0 +1,13 @@
+import math
+
+n = int(input())
+coord_list = [list(map(int, input().split())) for _ in range(n)]
+
+max_len = 0
+for i, p1 in enumerate(coord_list[:n - 1]):
+    for p2 in coord_list[i:n]:
+        temp_len = math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+        if temp_len > max_len:
+            max_len = temp_len
+
+print(max_len)
