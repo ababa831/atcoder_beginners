28d28e9f127d16b795fa3bd7fd6b7039ace3ace1
ababa893 <flvonlineconverter@gmail.com>
2019-02-16 04:13:36 -1000

    Add contest 118

---
+++ contest_118/b.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+leftover = None
+for i in range(n):
+    k_a_list = list(map(int, input().split()))
+    if i == 0:
+        leftover = k_a_list[1:]
+    else:
+        k = k_a_list[0]
+        leftover = [a for a in k_a_list[1:] if a in leftover]
+    if leftover == []:
+        print(0)
+        exit()
+
+print(len(leftover))
---
+++ contest_118/c.py
@@ -0,0 +1,13 @@
+from fractions import gcd 
+
+n = int(input())
+a_set = set(map(int, input().split()))
+max_a = max(a_set)
+min_sub = max_a
+
+for val in a_set:
+    min_sub = min(gcd(max_a, val), min_sub)
+    if min_sub == 1:
+        print(1)
+        exit()
+print(min_sub)
