b06e7b90e5ffeacaef3e5abaff2e147630551b70
ababa893 <flvonlineconverter@gmail.com>
2019-02-18 07:05:03 -1000

    In progress

---
+++ arihon_biginners/abc104_c.py
@@ -0,0 +1,8 @@
+# In progress
+D, G = map(int, input().split())
+
+for i in range(1, D+1):
+    p, c = map(int, input().split())
+    # bit full search for each p_i
+    for cand_i in range(1 << p)
+
