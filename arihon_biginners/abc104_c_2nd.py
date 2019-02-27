474e249cd93422d3fce68b95bde227284b17701a
ababa893 <flvonlineconverter@gmail.com>
2019-02-27 07:32:36 -1000

    WIP

--- arihon_biginners/abc104_c_2nd.py
+++ arihon_biginners/abc104_c_2nd.py
@@ -5,3 +5,8 @@ problems = [list(map(int, input().split())) for i in range(D)]
 # TODO:
 # bit search (1,,,,D)
 # May be ascending
+for i in range(1 << D):
+    for j in range(D):
+        # If all problems are solved
+        
+        # If all problems are not solved
