edc16c4d8f4169c9d38a64ca9aba824eed289513
ababa893 <flvonlineconverter@gmail.com>
2019-03-08 08:37:31 -1000

    WIP

--- arihon_biginners/abc104_c_2nd.py
+++ arihon_biginners/abc104_c_2nd.py
@@ -2,13 +2,12 @@
 D, G = map(int, input().split())
 problems = [list(map(int, input().split())) for i in range(D)]
 
-
 # bit search (1,,,,D)
 # May be ascending
-min_num_solved_probrems = 1000000
+min_num_solved_problems = 1000000
 for i in range(1 << D):
     tmp_score = 0
-    num_solved_probrems = 0
+    num_solved_problems = 0
     for j in range(D):
         # If all problems are solved
         if (i >> j) & 1:
@@ -16,11 +15,11 @@ for i in range(1 << D):
         # If all problems are not solved
         else:
             tmp_score += 0
-        num_solved_probrems += 1
+        num_solved_problems += 1
     # Judge if you reach the target score
-    if num_solved_probrems >= G:
-        min_num_solved_probrems = \
-            min(min_num_solved_probrems, num_solved_probrems)
+    if num_solved_problems >= G:
+        min_num_solved_problems = \
+            min(min_num_solved_problems, num_solved_problems)
     else:
         # TODO: write codes of solving problems halfway
         pass
