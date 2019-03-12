8333817a510499cbccb5cdbbddbc5421316b2e35
ababa893 <flvonlineconverter@gmail.com>
2019-03-12 05:22:35 -0900

    minor fix

--- arihon_biginners/abc104_c_2nd.py
+++ arihon_biginners/abc104_c_2nd.py
@@ -12,15 +12,12 @@ for i in range(1 << D):
         # If all problems are solved
         if (i >> j) & 1:
             tmp_score += j * 100 * problems[j][0] + problems[j][1]
-        # If all problems are not solved
-        else:
-            tmp_score += 0
-        num_solved_problems += 1
+        num_solved_problems += problems[j][0]
     # Judge if you reach the target score
     if num_solved_problems >= G:
         min_num_solved_problems = \
             min(min_num_solved_problems, num_solved_problems)
     else:
-        
+
         # TODO: write codes of solving problems halfway
         pass
