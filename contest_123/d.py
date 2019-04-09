599b1747959343ba2aab2d2a0bbef6eae3fdfba5
ababa893 <flvonlineconverter@gmail.com>
2019-04-09 07:28:49 -0900

    Add d

---
+++ contest_123/d.py
@@ -0,0 +1,6 @@
+# WIP
+x, y, z, k = map(int, input().split())
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+c_list = list(map(int, input().split()))
+# TODO: binary search
