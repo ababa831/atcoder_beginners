5e5c402c1a77bba830f7e988389a82e3c7a7d183
ababa893 <flvonlineconverter@gmail.com>
2019-05-01 09:37:57 -0900

    Add arc_031_b.py (WIP)

---
+++ arihon_biginners/arc_031_b.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+geo_map = [list(input()) for _ in range(10)]  # 10 x 10
+
+
+class IslandableChecker(object):
+    def __init__(self, geo_map, start_point=[0, 0]):
+        self.geo_map = geo_map
+        self.stack = deque()
+        self.start_point = start_point
+    
+    def check(self, redefined_start_point=None):
+        """
+        Check if the map can unite as a new island 
+        when a sea point is converted to a land one.
+        """
+        if redefined_start_point is not None:
+            self.start_point = redefined_start_point
+        # TODO:
+        # 1. Search "oxo"(two lands and a sea sandwitches)
+        # 2. Check if there are pairs which can reach each other via DFS
+        # 3. "The reachable pair" detected -> "YES", or -> "NO"
+
+
+    def count_land_pairs(self, sea_point):
+        pass
+
+    def has_land_pair(self, sea_point):
+        """
+        Check if there are pairs which describe 
+        "oxo"(two lands and a sea sandwitches)
+        """ 
+        pass
+    
+    def can_reach_each_other(self, pairs_list):
+        """Check if there are pairs which can reach each other via DFS"""
+        pass
+    
+    def dfs(self, dfs_start_point):
+        pass
