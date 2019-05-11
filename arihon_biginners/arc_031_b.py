686d8809558fd0417e6d7aa5a198a3754dea16e1
ababa893 <flvonlineconverter@gmail.com>
2019-05-11 08:57:04 -0900

    WIP

--- arihon_biginners/arc_031_b.py
+++ arihon_biginners/arc_031_b.py
@@ -15,14 +15,21 @@ class IslandableChecker(object):
         """
         if redefined_start_point is not None:
             self.start_point = redefined_start_point
+
         # TODO:
         # 1. Search "oxo"(two lands and a sea sandwitches)
-        # 2. Check if there are pairs which can reach each other via DFS
-        # 3. "The reachable pair" detected -> "YES", or -> "NO"
+        land_pairs = None
         for row_idx, row_val in enumerate(self.geo_map):
             for col_idx, col_val in enumerate(row_val):
                 if col_val == 'x':
-                    self.get_land_pairs(col_idx, row_idx)
+                    land_pairs = \
+                        self.get_land_pairs(col_idx, row_idx)
+
+        # 2. Check if there are pairs which can reach each other via DFS
+        # 3. "The reachable pair" detected -> "YES", or -> "NO"
+        for land_pair in land_pairs:
+            # WIP
+            pass
 
     def get_land_pairs(self, sea_point):
         """
@@ -35,7 +42,25 @@ class IslandableChecker(object):
             - Yes: pairs (e.g. [[(1, 2), (2, 3)]])
             - No: empty list
         """
-        pass
+        land_pairs = []
+
+        x, y = sea_point[0], sea_point[1]
+        # TODO:
+        # Check if the canditates exist
+        canditates1 = [self.geo_map[y + 1][x], self.geo_map[y - 1][x]]
+        canditates2 = [self.geo_map[y][x + 1], self.geo_map[y][x - 1]]
+
+        if self.is_land_pair(canditates1):
+            land_pairs.append([(y + 1, x), (y - 1, x)])
+        if self.is_land_pair(canditates2):
+            land_pairs.append([(y, x + 1), (y, x - 1)])
+        return land_pairs
+
+    def is_land_pair(self, canditates):
+        if canditates[0] == 'o' and canditates[1] == 'o':
+            return True
+        else:
+            return False
 
     def can_reach_each_other(self, pairs_list):
         """Check if there are pairs which can reach each other via DFS"""
