624ab4a84922c37fac172c627a37b4a021ca4c92
ababa893 <flvonlineconverter@gmail.com>
2019-05-21 04:37:04 -0900

    Update arc 031 2nd

--- arihon_biginners/arc_031_b_2nd.py
+++ arihon_biginners/arc_031_b_2nd.py
@@ -1,5 +1,69 @@
-# WIP
+# WA (WIP)
 # Memo:
 # There is
 # - a land -> 'YES'
-# - two or more lands -> If a sea point is next to edges of the lands
+# - two or more lands ->
+#   Convert a sea point to a land one
+#   and check if there is a land on the map using DFS
+from collections import deque
+
+
+class FillUpper(object):
+    def __init__(self, landsea_map, one_side=10):
+        self.stack = deque()
+        self.one_side = one_side
+        self.landsea_map = landsea_map
+        """
+        If DFS was finished and map_for_dfs == land_sea_map
+        -> 'YES'
+        """
+        self.map_for_dfs = [['x'] * one_side] * one_side
+
+    def fillup(self, point):
+        if not self._is_land_point(point):
+            self.landsea_map[point[1]][point[0]] = 'o'
+
+    def DFS(self):
+        point = self.stack.pop()
+        if not self._is_in_the_map(point):
+            return
+        elif not self._is_land_point(point):
+            return
+        else:
+            self.map_for_dfs[point[1]][point[0]] = 'o'
+            self.stack.append([point[0] + 1, point[1]])
+            self.stack.append([point[0] - 1, point[1]])
+            self.stack.append([point[0], point[1] + 1])
+            self.stack.append([point[0], point[1] - 1])
+
+    def _is_in_the_map(self, point):
+        in_the_xrange = 0 <= point[0] < self.one_side
+        in_the_yrange = 0 <= point[1] < self.one_side
+        if in_the_xrange and in_the_yrange:
+            return True
+        else:
+            return False
+
+    def _is_land_point(self, point):
+        if self.landsea_map[point[1]][point[0]] == 'o':
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    # 1. Input
+    one_side = 10
+    landsea_map = [list(input()) for _ in range(one_side)]
+    # 2. Full search and DFS to judge there is a land for every points
+    for x in range(one_side):
+        for y in range(one_side):
+            fu = FillUpper(landsea_map, one_side)
+            fu.fillup([x, y])
+            fu.stack.append([x, y])
+            while fu.stack:
+                fu.DFS()
+            if fu.map_for_dfs == fu.landsea_map:
+                print('YES')
+                exit()
+    print('NO')
