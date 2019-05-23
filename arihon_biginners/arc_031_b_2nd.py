f9cf977d2349af01bc65cfcf92797304143188dd
ababa893 <flvonlineconverter@gmail.com>
2019-05-23 06:26:33 -0900

    WIP (Still WA)

--- arihon_biginners/arc_031_b_2nd.py
+++ arihon_biginners/arc_031_b_2nd.py
@@ -5,7 +5,7 @@
 #   Convert a sea point to a land one
 #   and check if there is a land on the map using DFS
 from collections import deque
-# import time
+import time
 from copy import deepcopy
 import sys
 sys.setrecursionlimit(10**8)
@@ -22,10 +22,12 @@ class FillUpper(object):
         -> 'YES'
         """
         self.map_for_dfs = [['x'] * one_side] * one_side
+        self.n_fillupped = 0
 
     def fillup(self, point):
         if not self._is_land_point(point):
             self.landsea_map[point[1]][point[0]] = 'o'
+            self.n_fillupped += 1
 
     def DFS(self):
         point = self.stack.pop()
@@ -68,22 +70,27 @@ if __name__ == "__main__":
     # 2. Full search and DFS to judge there is a land for every points
     for x in range(one_side):
         for y in range(one_side):
-            fu = FillUpper(landsea_map, one_side)
+            map_for_fullsearch = deepcopy(landsea_map)
+            fu = FillUpper(map_for_fullsearch, one_side)
+            # print('n_stack: ', len(fu.stack))
             fu.fillup([x, y])
             filluped_map = deepcopy(fu.landsea_map)
 
-            if landsea_map[y][x] == 'x':
+            if map_for_fullsearch[y][x] == 'x':
                 errmsg = '埋め立てメソッドが有効に働いていない'
-                assert landsea_map != filluped_map, errmsg
+                assert map_for_fullsearch != filluped_map, errmsg
 
             fu.stack.append([x, y])
             while fu.stack:
                 fu.DFS()
-                # print('landsea_map:\n', fu.landsea_map)
-                # print('map_for_dfs:\n', fu.map_for_dfs)
-                # time.sleep(0.1)
-                
-            if fu.map_for_dfs == filluped_map:
+                """
+                # For debug
+                print('landsea_map:\n', fu.landsea_map)
+                print('map_for_dfs:\n', fu.map_for_dfs)
+                print('searched_points: ', fu.searched_points)
+                time.sleep(0.001)
+                """
+            if fu.map_for_dfs == filluped_map and fu.n_fillupped > 0:
                 print('YES')
                 exit()
 
