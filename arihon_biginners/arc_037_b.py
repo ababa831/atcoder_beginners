577e4fd64d40a054662024b66c3a735bb3aa1a52
ababa893 <flvonlineconverter@gmail.com>
2019-05-29 09:04:10 -0900

    WIP

--- arihon_biginners/arc_037_b.py
+++ arihon_biginners/arc_037_b.py
@@ -1,4 +1,7 @@
 # WIP
 N, M = map(int, input().split())
+# src (smaller num) -> dst (larger num)
 links = [tuple(map(int, input().split())) for _ in range(M)]
 
+# DFS:
+# IF there are sets of same number, the network is not tree.
