f2361fc8fa9b6f0137dfafe9181ad72130b4a67f
ababa893 <flvonlineconverter@gmail.com>
2019-06-11 07:44:03 -0900

    WIP

--- dp_practice/edp_b.py
+++ dp_practice/edp_b.py
@@ -4,5 +4,9 @@ hhh = list(map(int, input().split()))
 
 dp = [0] * N
 dp[0] = 0
-dp[1] = abs(hhh[1] - hhh[0])
-dp[2] = abs(hhh[2] - hhh[1]) + abs(hhh[1] - hhh[0]) + abs(hhh[2]-hhh[0])
+
+for i in range(1, N):
+    dp[i] = \
+        min([abs(hhh[i] - hhh[i - k]) + dp[i - k] for k in range(1, i + 1)])
+
+print(dp[-1])
