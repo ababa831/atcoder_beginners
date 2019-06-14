de8276904ed4417d412832a9ed0498344faa7969
ababa893 <flvonlineconverter@gmail.com>
2019-06-14 07:04:18 -0900

    Update EDPB

--- dp_practice/edp_b.py
+++ dp_practice/edp_b.py
@@ -1,17 +1,15 @@
-# Python3  -> TLE
-# PyPy3 -> AC
+# Python3
 N, K = map(int, input().split())
 hhh = list(map(int, input().split()))
 
 dp = [0] * N
 dp[0] = 0
 
-for i in range(1, N):
-    if i < K:
-        dp[i] = \
-            min([abs(hhh[i] - hhh[i-k]) + dp[i-k] for k in range(1, i+1)])
-    else:
-        dp[i] = \
-            min([abs(hhh[i] - hhh[i-k]) + dp[i-k] for k in range(1, K+1)])
+for i, h in enumerate(hhh):
+    s = 0 if i < K else i-K
+    if i == 0:
+        continue
+    dp[i] = \
+        min([abs(h - h_k) + dp_k for h_k, dp_k in zip(hhh[s:i], dp[s:i])])
 
 print(dp[-1])
