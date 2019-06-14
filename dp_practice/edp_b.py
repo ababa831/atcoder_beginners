50af300f9905430f95f41ae0840918999bc40f40
ababa893 <flvonlineconverter@gmail.com>
2019-06-13 20:43:37 -0900

    Updated

--- dp_practice/edp_b.py
+++ dp_practice/edp_b.py
@@ -1,4 +1,5 @@
-# WIP
+# Python3  -> TLE
+# PyPy3 -> AC
 N, K = map(int, input().split())
 hhh = list(map(int, input().split()))
 
@@ -6,7 +7,11 @@ dp = [0] * N
 dp[0] = 0
 
 for i in range(1, N):
-    dp[i] = \
-        min([abs(hhh[i] - hhh[i - k]) + dp[i - k] for k in range(1, i + 1)])
+    if i < K:
+        dp[i] = \
+            min([abs(hhh[i] - hhh[i-k]) + dp[i-k] for k in range(1, i+1)])
+    else:
+        dp[i] = \
+            min([abs(hhh[i] - hhh[i-k]) + dp[i-k] for k in range(1, K+1)])
 
 print(dp[-1])
