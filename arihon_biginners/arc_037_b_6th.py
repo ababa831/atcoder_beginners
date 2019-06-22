a8bc781b8076912b432b97b3b3c2d42d605a2fd9
ababa893 <flvonlineconverter@gmail.com>
2019-06-22 08:05:03 -0900

    AC

--- arihon_biginners/arc_037_b_6th.py
+++ arihon_biginners/arc_037_b_6th.py
@@ -1,62 +1,55 @@
-# WA
 from collections import deque
-
+ 
 N, M = map(int, input().split())
-
+ 
 connect = [[] for _ in range(N)]
 for _ in range(M):
     u, v = map(int, input().split())
     connect[u - 1].append(v)
-    # connect[v - 1].append(u)
-
+    connect[v - 1].append(u)
+ 
 # print(connect)
-
+ 
 # Global settings
 already_passed = [False] * N
 queue = deque()
 is_tree = True
 max_in_graph = 1
-
-
+ 
+ 
 def DFS():
     # Global variables
     global already_passed
     global queue
     global is_tree
-    global max_in_graph
-    
-    curr_node = queue.popleft()
-
-    if already_passed[curr_node - 1]:
-        is_tree = False
-    else:
-        nexts = connect[curr_node - 1]
-
-        curr_max = -1
-        if len(nexts) != 0:
-            curr_max = max(nexts)
-            for n in nexts:
-                queue.append(n)
+ 
+    prev_node, curr_node = queue.popleft()
+    already_passed[curr_node - 1] = True
+ 
+    next_nodes = connect[curr_node - 1]
+    for next_node in next_nodes:
+        if next_node == prev_node:
+            continue
+        elif already_passed[next_node - 1]:
+            is_tree = False
+            return
         else:
-            curr_max = curr_node
-
-        already_passed[curr_node - 1] = True
-        max_in_graph = max(curr_max, max_in_graph)
-
-
+            queue.append([curr_node, next_node])
+ 
+ 
 n_tree = 0
-jump_to = 1
-while jump_to <= N:
-    queue.append(jump_to)
-    while queue:
-        # print(queue)
-        DFS()
-        # print(queue)
-        # print(is_tree)
-    if is_tree:
-        n_tree += 1
+prev = -1
+for i in range(1, N + 1):
+    if not already_passed[i - 1]:
+        queue.append([prev, i])
+        while queue:
+            # print(queue)
+            DFS()
+            # print(queue)
+            # print(is_tree)
+        if is_tree:
+            n_tree += 1
     # Reset settings
     is_tree = True
-    jump_to = max_in_graph + 1
-
+ 
 print(n_tree)
