f1106d8b122c3eeaffc978f393c686529191d588
ababa893 <flvonlineconverter@gmail.com>
2019-07-01 07:31:19 -0900

    AC

---
+++ contest_122/c.py
@@ -0,0 +1,13 @@
+# AC
+N, Q = map(int, input().split())
+S = input()
+
+t = [0] * (N + 2)
+t[0] = 1 if S[:2] == 'AC' else 0
+
+for i in range(N - 2):
+    t[i + 1] = t[i] + (1 if S[i:i + 2] == 'AC' else 0)
+
+for _ in range(Q):
+    l, r = map(int, input().split())
+    print(t[r - 1] - t[l - 1])
