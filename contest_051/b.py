85128574d3df7e8b367ccd6d78b20f1d5045cad2
ababa893 <flvonlineconverter@gmail.com>
2019-07-06 09:31:32 -0900

    AC

---
+++ contest_051/b.py
@@ -0,0 +1,11 @@
+# AC
+K, S = map(int, input().split())
+
+n_combinations = 0
+for x in range(K+1):
+    for y in range(K+1):
+        z = S - x - y
+        if 0 <= z <= K:
+            n_combinations += 1
+
+print(n_combinations)
