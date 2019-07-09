18c47ef2a59cbbea4c8574ae3565c7fc0dadc6a4
ababa893 <flvonlineconverter@gmail.com>
2019-07-09 05:36:25 -0900

    review

---
+++ contest_126/c_2nd.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+
+sum_p = 0
+for i in range(1, N+1):
+    if i >= K:
+        sum_p += 1/N
+    else:
+        n_powers = 1
+        while True:
+            if i * (2 ** n_powers) >= K:
+                break
+            n_powers += 1
+        sum_p += 1/N * 0.5 ** n_powers
+
+print(sum_p)
