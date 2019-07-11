5d552fd274ecc5ca14863c6c6ba7dab88178e490
ababa893 <flvonlineconverter@gmail.com>
2019-07-11 07:57:43 -0900

    AC

---
+++ contest_124/c_2nd.py
@@ -0,0 +1,14 @@
+S = list(map(int, list(input())))
+
+last = S[0]
+n_change = 0
+for idx, current in enumerate(S):
+    if idx == 0:
+        continue
+    if current == last:
+        current = 0 if current == 1 else 1
+        S[idx] = current
+        n_change += 1
+    last = current
+
+print(n_change)
