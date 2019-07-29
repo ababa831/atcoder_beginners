613248ee0c7786d33daeca87556d4a71d9410b17
ababa893 <flvonlineconverter@gmail.com>
2019-07-29 08:41:21 -0900

    WIP

--- contest_117/c.py
+++ contest_117/c.py
@@ -2,6 +2,13 @@
 # BFS?
 N, M = map(int, input().split())
 XXX = list(map(int, input().split()))
+XXX.sort()
+
+dists = [abs(XXX[i] - XXX[i+1]) for i in range(M) if i < M-1]
+already_passed = [False] * M
+
 
 # Sort -> place komas on the two edges 
 # -> calc distance between elements in sorted XXX
+
+
