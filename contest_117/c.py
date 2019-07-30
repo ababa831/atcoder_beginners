a68f2839390fe4c5ff50247b454cd42064d21400
ababa893 <flvonlineconverter@gmail.com>
2019-07-30 07:37:45 -0900

    AC

--- contest_117/c.py
+++ contest_117/c.py
@@ -1,14 +1,15 @@
-# WIP
-# BFS?
+# AC
 N, M = map(int, input().split())
+
+if N >= M:
+    print(0)
+    exit()
+
 XXX = list(map(int, input().split()))
 XXX.sort()
 
-dists = [abs(XXX[i] - XXX[i+1]) for i in range(M) if i < M-1]
-already_passed = [False] * M
+sections = [abs(XXX[i] - XXX[i + 1]) for i in range(M) if i < M - 1]
+sections.sort(reverse=True)
+not_arrived = sum(sections[:N-1])
 
-
-# Sort -> place komas on the two edges 
-# -> calc distance between elements in sorted XXX
-
-
+print((XXX[-1] - XXX[0]) - not_arrived)
