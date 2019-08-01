fe4c3275c205bf11cde2f8c58275a06d98f99630
ababa893 <flvonlineconverter@gmail.com>
2019-08-01 09:11:27 -0900

    WI

--- contest_116/c.py
+++ contest_116/c.py
@@ -4,30 +4,42 @@ from collections import deque
 N = int(input())
 hhh = list(map(int, input().split()))
 
-"""
 n_showered = 0
 max_hhh = max(hhh)
 
 queue = deque()
-queue.append(hhh)
 
-while queue:
+
+def enqueue_wo_zero_elems(target_list):
+    global queue, n_showered
     zero_exists = True
     cursor = -1
-    target_list = queue.popleft()
-    target_list = list(map(lambda x: x-1, target_list))
-    n_showered += 1
-
     while zero_exists:
-        if cursor >= N-1:
-            break
+        # print(zero_exists, cursor, queue)
         try:
-            cursor = target_list.index(0)
-            if len(target_list[:cursor]) != 0:
-                queue.append(target_list[:cursor])
-            target_list = target_list[cursor+1:]
+            if cursor < len(target_list) - 1:
+                zero_idx_leftmost = target_list[cursor + 1:].index(0)
+                part_list = target_list[:zero_idx_leftmost]
+                # print(part_list)
+                if len(part_list) != 0:
+                    part_list = list(map(lambda x: x - 1, part_list))
+                    n_showered += 1
+                    queue.append(part_list)
+                cursor = zero_idx_leftmost
+            else:
+                zero_exists = False
         except ValueError:
+            leftover = target_list[cursor + 1:]
+            if len(leftover) != 0:
+                queue.append(leftover)
             zero_exists = False
 
+
+# Initial queing
+enqueue_wo_zero_elems(hhh)
+
+while queue:
+    target_list = queue.popleft()
+    enqueue_wo_zero_elems(target_list)
+
 print(n_showered)
-"""
