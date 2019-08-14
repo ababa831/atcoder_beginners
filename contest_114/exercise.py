dbe1824776f415d58cb19edf076fc73a5b916d20
ababa893 <flvonlineconverter@gmail.com>
2019-08-14 20:17:12 +0900

    WIP

--- contest_114/exercise.py
+++ contest_114/exercise.py
@@ -14,30 +14,39 @@ N_str = input()
 len_N = len(N_str)
 N = int(N_str)
 
-dp = [[-1, -1] for _ in range(len_N)]  # dp[digit][smaller]
-
-# Initial settings
-digit_num = N_str[0]
-if digit_num > 5:
-    # X, 6, ..., 4, 3, 2, 1
-    dp[0][0] = 1
-    dp[0][1] = digit_num - 2
-elif digit_num == 5:
-    dp[0][0] = 0
-    dp[0][1] = 4
-else:
-    dp[0][0] = 1
-    dp[0][1] = digit_num - 1
-
-for i in range(len_N):
-    digit_num = N_str[i]
-    if digit_num > 5:
-        # X, 6, ..., 4, 3, 2, 1, "0"
-        dp[i+1][0] = 1 * dp[i][0]
-        dp[i+1][1] = (digit_num - 2) * dp[i][0] + 10 * dp[i][1]
-    elif digit_num == 5:
-        dp[i+1][0] = 0
-        dp[i+1][1] = 4
+dp = [[-1, -1] for _ in range(len_N)]  # dp[digit][is_max]
 
 
+def update(digit, is_max):
+    num_next, num_next_0, num_next_1 = None, None, None
+    if is_max == 0:
+        num_next = 9
+        is_max_next = 0
+        return num_next, is_max_next
+    else:
+        if digit > 5:
+            num_next_0 = digit-1
+            num_next_1 = 1
+        else:
+            num_next_0 = digit
+            num_next_1 = 1
+        is_max_next_0, is_max_next_1 = 0, 1
+        return num_next_0, is_max_next_0, num_next_1, is_max_next_1
 
+
+digit_0 = int(N_str[0])
+dp[0][0] = digit_0-1 if digit_0 > 5 else digit_0
+dp[0][1] = 1
+
+for i, dp_v in enumerate(dp[:-1]):
+    digit = int(N_str[i])
+    num_next_0_frm0, is_max_next_0_frm0 = update(digit, 0)
+    num_next_0_frm1, is_max_next_0_frm1, num_next_1, is_max_next_1 = \
+        update(digit, 1)
+
+    print(dp_v)
+    dp[i+1][1] = num_next_1 * dp_v[1]
+    dp[i+1][0] = num_next_0_frm0 * dp_v[0] + num_next_0_frm1 * dp_v[1]
+
+# Show result
+print(dp[-1][0] + dp[-1][1])
