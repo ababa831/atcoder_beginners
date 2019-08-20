dc865df92e9af3f59e76d2cd72ade576c552099e
ababa893 <flvonlineconverter@gmail.com>
2019-08-20 07:28:57 -0900

    AC/WA

--- contest_138/d.py
+++ contest_138/d.py
@@ -1,43 +1,33 @@
-# TLE
-from collections import deque
+# AC (test cases assigned before contest)
+# WA (after_contest_x)
+import sys
 
+input = sys.stdin.readline
 
 N, Q = map(int, input().split())
 
 link = [None] * N
-for _ in range(N-1):
+for _ in range(N - 1):
     a, b = map(int, input().split())
-    if link[a-1] is None:
-        link[a-1] = [b]
+    if link[a - 1] is None:
+        link[a - 1] = [b]
     else:
-        link[a-1].append(b)
+        link[a - 1].append(b)
 
 states = [0] * N
-stack = deque()
-
-
-def dfs(x):
-    p = stack.popleft()
-
-    # Add score
-    states[p-1] += x
-    # Search nexts
-    nexts = link[p-1]
-    if nexts is None:
-        return
-    for next_node in nexts:
-        stack.append(next_node)
-
-
-def update(p, x):
-    stack.append(p)
-    while stack:
-        dfs(x)
-
 
 for _ in range(Q):
     p, x = map(int, input().split())
-    update(p, x)
+    states[p - 1] += x
+
+for i, state in enumerate(states):
+    if state == 0:
+        continue
+    next_nodes = link[i]
+    if next_nodes is None:
+        continue
+    for node in next_nodes:
+        states[node - 1] += state
 
 # Output
-print(' '.join(map(str, states)))
+print(*states)
