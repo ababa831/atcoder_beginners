e1233935f35cf677b703691ac05e7c98fb7c00b0
ababa893 <flvonlineconverter@gmail.com>
2019-09-03 10:19:38 -0900

    WIP

--- contest_113/c_id.py
+++ contest_113/c_id.py
@@ -4,4 +4,29 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-py_list = [tuple(map(int, input().split())) for m in range(m)]
+py_list = [list(map(int, input().split()))+[i] for i, m in enumerate(range(m))]
+py_list.sort(key=lambda x: x[1])
+
+cursor_p = 1
+current = 0
+city = None
+addresses = []
+for py in py_list:
+    p = py[0]
+    y = py[1]
+    idx = py[2]
+
+    if p == cursor_p:
+        current += 1
+    elif p > cursor_p:
+        current = 1
+        cursor_p = p
+    city = str(current).zfill(6)
+    pref = str(cursor_p).zfill(6)
+
+    addresses.append((idx, pref+city))
+
+addresses.sort(key=lambda x: x[0])
+
+for address in addresses:
+    print(address[1])
