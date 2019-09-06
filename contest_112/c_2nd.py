c64486c5c0c696b0e8cb5cdf93bfa7728298b481
ababa893 <flvonlineconverter@gmail.com>
2019-09-06 10:13:09 -0900

    WIP

--- contest_112/c_2nd.py
+++ contest_112/c_2nd.py
@@ -4,3 +4,9 @@ input = sys.stdin.readline
 
 N = int(input())
 xyh_list = [tuple(map(int, input().split())) for _ in range(N)]
+
+"""memo:
+for Cx, Cy in zip(range(0, 101), range(0, 101))
+hi + abs(Xi-Cx)+abs(Yi-Cy) == hj + abs(Xj-Cx)+abs(Yj-Cy)
+in All points
+"""
