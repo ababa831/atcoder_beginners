6743d12c04dc3fe85794c5a8daea274f619fcb5b
ababa893 <flvonlineconverter@gmail.com>
2019-09-06 10:14:22 -0900

    WIP

--- contest_112/c_2nd.py
+++ contest_112/c_2nd.py
@@ -9,4 +9,6 @@ xyh_list = [tuple(map(int, input().split())) for _ in range(N)]
 for Cx, Cy in zip(range(0, 101), range(0, 101))
 hi + abs(Xi-Cx)+abs(Yi-Cy) == hj + abs(Xj-Cx)+abs(Yj-Cy)
 in All points
+
+Complexity: O(N^2) <= 10000
 """
