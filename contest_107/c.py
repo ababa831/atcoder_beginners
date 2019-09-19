9d5f66b2ec4614ec0184a27d0cb826abd6160d33
ababa893 <flvonlineconverter@gmail.com>
2019-09-20 01:57:44 +0900

    AC

--- contest_107/c.py
+++ contest_107/c.py
@@ -1,41 +1,18 @@
-# WIP (WA: 1_01.txt, 1_06.txt, 1_07.txt)
+# AC
 import sys
 input_s = sys.stdin.readline
 
 N, K = map(int, input_s().split())
 xxx = list(map(int, input_s().split()))
-if K == 1:
-    xxx = [abs(x) for x in xxx]
-    print(min(xxx))
-    exit()
 
-# Select candles
-min_distance = 10000000000
-arg_min = 100000000
-for i in range(N-K+1):
-    left_edge, right_edge = xxx[i], xxx[i+K-1]
-    tmp_larger = max(abs(left_edge), abs(right_edge))
-    if tmp_larger < min_distance:
-        arg_min = i
-        min_distance = tmp_larger
-    elif tmp_larger == min_distance and left_edge * right_edge > 0:
-        # If all elements of an extracted list
-        # are smaller than 0 or larger than 0
-        arg_min = i
-        min_distance = tmp_larger
+min_elapsed_time = 10**10
+for left, right in zip(xxx, xxx[K - 1:]):
+    elapsed_time = right - left + min(abs(left), abs(right))
+    """
+    right - left : a section distance
+    min(abs(left), abs(right)) : a start point to move the section
+    """
+    min_elapsed_time = \
+        elapsed_time if elapsed_time < min_elapsed_time else min_elapsed_time
 
-
-# Select route
-candles = xxx[arg_min:arg_min+K]
-left_edge = candles[0]
-right_edge = candles[-1]
-left_edge_dist = abs(left_edge)
-right_edge_dist = abs(right_edge)
-if left_edge > 0:
-    print(right_edge)
-elif right_edge < 0:
-    print(abs(left_edge))
-elif left_edge_dist > right_edge_dist:
-    print(right_edge_dist*2+left_edge_dist)
-else:
-    print(left_edge_dist*2+right_edge_dist)
+print(min_elapsed_time)
