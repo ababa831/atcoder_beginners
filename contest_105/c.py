f8258fc37c1e5ced9ed43db90504068edc8b1d17
ababa893 <flvonlineconverter@gmail.com>
2019-09-27 02:49:23 +0900

    WIP

--- contest_105/c.py
+++ contest_105/c.py
@@ -1,11 +1,15 @@
 from math import log2
 
 N = int(input())
-if N % 2:
-    search_lower = int(log2(abs(N) + 1))
+digit_canditate1 = int(log2(abs(N)))
+digit_canditate2 = digit_canditate1 - 1
+
+digit = None
+if N < 0:
+    digit = digit_canditate1 if digit_canditate1 % 2 else digit_canditate2
 else:
-    search_upper = int(log2(abs(N)))
+    digit = digit_canditate2 if digit_canditate2 % 2 else digit_canditate1
 
-search_upper = search_lower + 1
-
-# WIP
+ans_canditates = [[0, 0] for _ in range(2**digit)]
+# {0, 1} * (-2) ** k + ...
+# TODO: Calculation using DP
