35de88a553bbaadf0ea1653f27e0d3238b9be01b
ababa893 <flvonlineconverter@gmail.com>
2019-09-28 03:20:26 +0900

    WIP

--- contest_104/c.py
+++ contest_104/c.py
@@ -7,3 +7,13 @@ D, G = map(int, input().split())
 pc_list = [tuple(map(int, input().split())) for _ in range(D)]
 
 # WIP
+temp_min_score = 0
+for i in range(1 << len(D)):
+    score_per_combi = 0
+    for j in range(len(D)):
+        if (i >> j) & 1:
+            p = pc_list[j][0]
+            c = pc_list[j][1]
+            score_per_combi += (j + 1) * 100 * p + c
+    if score_per_combi >= G:
+        temp_min_score = min(temp_min_score, score_per_combi)
