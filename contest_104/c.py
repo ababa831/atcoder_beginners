e7880bf7cf7be274fe4ef96d669bd66f06a01567
ababa893 <flvonlineconverter@gmail.com>
2019-09-30 02:13:30 +0900

    WIP

--- contest_104/c.py
+++ contest_104/c.py
@@ -10,10 +10,14 @@ pc_list = [tuple(map(int, input().split())) for _ in range(D)]
 temp_min_score = 0
 for i in range(1 << len(D)):
     score_per_combi = 0
+    combi = []
+    combi_min = []
     for j in range(len(D)):
         if (i >> j) & 1:
             p = pc_list[j][0]
             c = pc_list[j][1]
             score_per_combi += (j + 1) * 100 * p + c
+            combi.append(j)
     if score_per_combi >= G:
         temp_min_score = min(temp_min_score, score_per_combi)
+        combi_min = combi
