c306557b2da7775554f317f4fc47f224354cd4ee
ababa893 <flvonlineconverter@gmail.com>
2019-10-02 23:37:35 +0900

    WIP (WA)

--- contest_104/c.py
+++ contest_104/c.py
@@ -1,23 +1,47 @@
+
+# WIP, (WA)
 import sys
-from itertools import combinations
 
 sys_input = sys.stdin.readline
 
-D, G = map(int, input().split())
-pc_list = [tuple(map(int, input().split())) for _ in range(D)]
+D, G = map(int, sys_input().split())
+pc_list = [tuple(map(int, sys_input().split())) for _ in range(D)]
 
-# WIP
-temp_min_score = 0
-for i in range(1 << len(D)):
+temp_min_score = 10000000000000000
+combi_min = []
+n_solved = 0
+for i in range(1 << D):
     score_per_combi = 0
     combi = []
-    combi_min = []
-    for j in range(len(D)):
+    tmp_n_solved = 0
+    for j in range(D):
         if (i >> j) & 1:
             p = pc_list[j][0]
             c = pc_list[j][1]
             score_per_combi += (j + 1) * 100 * p + c
             combi.append(j)
-    if score_per_combi >= G:
-        temp_min_score = min(temp_min_score, score_per_combi)
+            tmp_n_solved += p
+    if score_per_combi >= G and score_per_combi < temp_min_score:
+        temp_min_score = score_per_combi
         combi_min = combi
+        n_solved = tmp_n_solved
+
+# problems are already sorted
+# -> check the problems were oversolved
+tmp_reduced = temp_min_score
+for problem_idx in combi_min:
+    for solved_num in range(1, pc_list[problem_idx][0] + 1):
+        if solved_num == 1:
+            c = pc_list[problem_idx][1]
+            diff = (problem_idx + 1) * 100 + c
+            tmp_reduced -= diff
+        else:
+            diff = (problem_idx + 1) * 100
+            tmp_reduced -= diff
+        n_solved -= 1
+
+        if tmp_reduced < G:
+            print(n_solved + 1)
+            exit()
+
+print(n_solved)
