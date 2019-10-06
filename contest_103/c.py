974bbfec55ea2d26b3bd3003df08e8eca7197e55
ababa893 <flvonlineconverter@gmail.com>
2019-10-06 23:58:23 +0900

    WIP

--- contest_103/c.py
+++ contest_103/c.py
@@ -5,3 +5,26 @@ Simple search
 
 Need to find a solution with less complexity
 """
+# May be TLE
+from fractions import gcd
+import sys
+
+
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
+
+# Input
+N = input()
+aaa = list(map(int, sys.stdin.readline().split()))
+
+# Solve total LCM
+total_lcm = 1
+for a in aaa:
+    total_lcm = lcm(total_lcm, a)
+
+# Find max f(m)
+max_f = 0
+for m_canditate in range(1, total_lcm+1):
+    f = 0
+    for a in aaa:
+        f += 
