7f5fddc7d497cdcfc333d28d9f863393b48dfcf1
ababa893 <flvonlineconverter@gmail.com>
2019-10-07 00:05:49 +0900

    AC

--- contest_103/c.py
+++ contest_103/c.py
@@ -1,30 +1,12 @@
-"""[summary]
-Simple search
--> Complexity: 10**5 * 3000
--> TLE
-
-Need to find a solution with less complexity
-"""
-# May be TLE
-from fractions import gcd
+# AC
+# I don't know why the logic works correctly
 import sys
 
-
-def lcm(x, y):
-    return (x * y) // gcd(x, y)
-
 # Input
-N = input()
+N = int(input())
 aaa = list(map(int, sys.stdin.readline().split()))
 
-# Solve total LCM
-total_lcm = 1
-for a in aaa:
-    total_lcm = lcm(total_lcm, a)
+sum_aaa = sum(aaa)
 
-# Find max f(m)
-max_f = 0
-for m_canditate in range(1, total_lcm+1):
-    f = 0
-    for a in aaa:
-        f += 
+# Outout
+print(sum_aaa - N)
