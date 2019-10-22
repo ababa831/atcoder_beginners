9f2d8587c752aa81ff1f0b3aefaf7083730edc39
ababa893 <flvonlineconverter@gmail.com>
2019-10-22 23:59:36 +0900

    WIP

--- contest_100/c.py
+++ contest_100/c.py
@@ -3,3 +3,22 @@ import sys
 sys_input = sys.stdin.readline
 N = int(sys_input())
 aaa = list(map(int, sys_input().split()))
+
+# If all values are odd
+aaa_remain = set(map(lambda x: x % 2, aaa))
+if len(aaa_remain) == 1 and sum(aaa_remain) == 1:
+    print(0)
+    exit()
+
+aaa_even = [a for a in aaa if a % 2 == 0]
+target_a = max(aaa_even)
+
+n_divide = 0
+is_dividable = True
+while is_dividable:
+    is_dividable = False if target_a % 2 else True
+    if is_dividable:
+        target_a = target_a//2
+        n_divide += 1
+
+print(n_divide)
