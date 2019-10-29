ebf1f42f0d3ef45cdfc6653aef94aa9f8cb04379
ababa893 <flvonlineconverter@gmail.com>
2019-10-29 23:19:25 +0900

    WIP(The fucking code)

--- contest_099/c.py
+++ contest_099/c.py
@@ -1,34 +1,42 @@
-# WIP
+# WIP（The fucking code）
 from math import log
 N = int(input())
-log_N = log(N)
+# log_N = log(N)
 
 
-def search_range(target_yen, n):
+def search_range(target_yen, remaining):
     x = 1
     log_value = log(target_yen)
-    log_n = log(n)
+    log_n = log(remaining)
     while log_n > log_value:
         x += 1
         log_value = x * log(target_yen)
     return x - 1, x
 
 
-# 9
-min_x, max_x = search_range(9, N)
-log_max = max_x * log(9)
-log_min = min_x * log(9)
-if log_N == log_max:
-    print(max_x)
-    exit()
-else:  # log_N < log_max
-    # 6
-    remaining = N - 9 ** min_x
-    min_x_2, max_x_2 = search_range(6, remaining)
-    log_remaining = log(remaining)
-    log_max_2 = max_x_2 * log(6)
-    log_min_2 = min_x_2 * log(6)
-    if log_remaining == log_max_2:
-        print(min_x+max_x_2)
+def get_selected_power(idx_canditate, power_9, power_6):
+    if idx_canditate == 0:
+        return power_9
+    elif idx_canditate == 1:
+        return power_6
     else:
-        print(N) if min_x == 0 and min_x_2 == 0 else print(min_x+min_x_2)
+        return 1
+
+
+n_operation = 0
+remaining = N
+while remaining > 0:
+    power_9, _ = search_range(6, remaining)
+    power_6, _ = search_range(9, remaining)
+    canditate = [9**power_9, 6**power_6, 1]
+    sub_number = max([9**power_9, 6**power_6, 1])
+    idx_cand = canditate.index(sub_number)
+    selected_power = get_selected_power(idx_cand, power_9, power_6)
+    if selected_power == 0:
+        selected_power = 1
+
+    remaining -= sub_number
+    n_operation += selected_power
+    # print(remaining, selected_power, sub_number)
+
+print(n_operation)
