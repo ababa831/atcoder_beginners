1a23c77d756fe4bebee6b09a698524514979ad26
ababa893 <flvonlineconverter@gmail.com>
2019-11-03 02:29:40 +0900

    WIP

--- contest_099/c.py
+++ contest_099/c.py
@@ -1,41 +1,40 @@
 # WIP (The fucking code)
-from math import log
 N = int(input())
 
 
 def search_range(target_yen, remaining):
     x = 1
-    log_value = log(target_yen)
-    log_n = log(remaining)
-    while log_n > log_value:
+    exp_val = target_yen ** x
+    while remaining > exp_val:
         x += 1
-        log_value = x * log(target_yen)
-    return x - 1, x
-
-
-def get_selected_power(idx_canditate, power_9, power_6):
-    if idx_canditate == 0:
-        return power_9
-    elif idx_canditate == 1:
-        return power_6
-    else:
-        return 1
+        exp_val = target_yen ** x
+    return x - 1
 
 
 n_operation = 0
 remaining = N
+inf = float('inf')
 while remaining > 0:
-    power_9, _ = search_range(9, remaining)
-    power_6, _ = search_range(6, remaining)
-    canditate = [9**power_9, 6**power_6, 1]
-    sub_number = max([9**power_9, 6**power_6, 1])
-    idx_cand = canditate.index(sub_number)
-    selected_power = get_selected_power(idx_cand, power_9, power_6)
-    if selected_power == 0:
-        selected_power = 1
+    power_9 = search_range(9, remaining)
+    power_6 = search_range(6, remaining)
+    canditate = None
+    if power_9 == 0 and power_6 == 0:
+        canditate = [inf, inf, remaining]
+    elif power_9 == 0:
+        canditate = [inf, remaining - 6**power_6, remaining]
+    elif power_6 == 0:
+        canditate = [remaining - 9**power_9, inf, remaining]
+    else:
+        canditate = [remaining - 9**power_9, remaining - 6**power_6, remaining]
 
-    remaining -= sub_number
-    n_operation += selected_power
-    print(remaining, selected_power, sub_number)
+    # Update remaining
+    remaining = min(canditate)
+    argmin_cand = canditate.index(remaining)
+    target_power = remaining if argmin_cand == 2 else 1
+    # print(canditate, argmin_cand, target_power, remaining)
+    n_operation += target_power
+    if argmin_cand == 2:
+        break
 
+# print('========')
 print(n_operation)
