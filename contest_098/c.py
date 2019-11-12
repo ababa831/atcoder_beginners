c5f5645bbc32e2ea03081cbd7d46a5d3aaf6a4e8
ababa893 <flvonlineconverter@gmail.com>
2019-11-12 23:58:13 +0900

    AC

--- contest_098/c.py
+++ contest_098/c.py
@@ -1,4 +1,4 @@
-# WIP (Cumulative sum?)
+# AC (Cumulative sum?)
 N = int(input())
 S = input()
 
@@ -6,12 +6,13 @@ num_E, num_W = 0, 0
 # num_E_list = [0] * (N + 1)
 # num_W_list = [0] * (N + 1)
 min_sum_operation = N
+argmin_i = 0
 for i, c in enumerate(S):
     if i == 0:
         # This operation is safe because N is larger than 2
         right = S[i + 1:]
         num_E = right.count('E')
-        num_W = 0
+        num_W += 1 if c == 'W' else 0
         # num_E_list[i] = num_E
         # num_W_list[i] = num_W
     elif c == 'W':
@@ -20,6 +21,13 @@ for i, c in enumerate(S):
     elif c == 'E':
         num_E -= 1
         # num_E_list[i] = num_E
-    min_sum_operation = min(min_sum_operation, num_E + num_W)
+    if min_sum_operation > num_E + num_W:
+        min_sum_operation = min(min_sum_operation, num_E + num_W)
+        argmin_i = i
 
-print(min_sum_operation)
+    # print(min_sum_operation, num_E, num_W)
+
+if argmin_i == 0:
+    print(min_sum_operation - 1)
+else:
+    print(min_sum_operation)
