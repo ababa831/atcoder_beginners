cbcfbad0603f50ef5298a12bcea61317e577cf87
ababa893 <flvonlineconverter@gmail.com>
2019-11-27 01:25:33 +0900

    AC

--- contest_097/c.py
+++ contest_097/c.py
@@ -1,5 +1,25 @@
-# WIP (? 5000! -> inf)
-# I will skip this problem
-# because I coudn't get the meaning of "substring" order
+# AC
 s = input()
 K = int(input())
+
+minimum_canditates = sorted(set(s))
+n_counted = 0
+
+canditates = []
+
+
+def add_canditate(target_chara):
+    global n_counted
+
+    canditates.append(target_chara)
+    n_counted += 1
+    for c in minimum_canditates:
+        next_target = target_chara + c
+        if n_counted < K and next_target in s:
+            add_canditate(next_target)
+
+
+for c in minimum_canditates:
+    add_canditate(c)
+
+print(canditates[K - 1])
