59d433ddedd3891f4fcfa8e168c7a75b7e8ca947
ababa893 <flvonlineconverter@gmail.com>
2019-12-05 06:18:20 -1000

    WIP (Wrong pattern)

--- contest_096/c.py
+++ contest_096/c.py
@@ -5,6 +5,36 @@ from collections import deque
 sys_input = sys.stdin.readline
 
 H, W = map(int, sys_input().split())
-areamap = [list(sys_input().replace('\n', '')) for h in range(H)]
+
+detected = False
+start_point = None
+areamap = []
+for h in enumerate(range(H)):
+    row = list(sys_input().replace('\n', ''))
+    if not detected:
+        w = row.index('#')
+        start_point = [h, w]
+        detected = True
+    areamap.append(row)
 
 stack = deque()
+stack.append(start_point)
+
+painted = [False for _ in range(W)] * H
+
+
+def dfs():
+    h_curr, w_curr = stack.pop()
+    if 0 > h_curr <= H and 0 <= w_curr <= W:
+        return
+
+    mark_curr = areamap[h_curr][w_curr]
+    state_curr = painted[h_curr][w_curr]
+    if not state_curr and mark_curr == '#':
+        painted[h_curr][w_curr] = True
+        """NOTE: Wrong pattern
+        stack.append(h_curr + 1, w_curr)
+        stack.append(h_curr - 1, w_curr)
+        stack.append(h_curr, w_curr + 1)
+        stack.append(h_curr, w_curr - 1)
+        """
