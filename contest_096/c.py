bf29f2d6ce988252dbde2bae9023c481ae4d0f38
ababa893 <flvonlineconverter@gmail.com>
2019-12-10 15:17:28 +0900

    AC

--- contest_096/c.py
+++ contest_096/c.py
@@ -1,40 +1,27 @@
-# WIP (DFS?, example 3 ?)
+# AC (I mixed up the problem intent)
 import sys
-from collections import deque
+
 
 sys_input = sys.stdin.readline
 
 H, W = map(int, sys_input().split())
 
-detected = False
-start_point = None
 areamap = []
+areamap.append(['.']*(W+2))
 for h in enumerate(range(H)):
     row = list(sys_input().replace('\n', ''))
-    if not detected:
-        w = row.index('#')
-        start_point = [h, w]
-        detected = True
+    row = ['.'] + row + ['.']
     areamap.append(row)
+areamap.append(['.']*(W+2))
 
-stack = deque()
-stack.append(start_point)
+for h in range(1, H+1):
+    for w in range(1, W+1):
+        if areamap[h][w] == '#' and \
+                areamap[h][w+1] == '.' and \
+                areamap[h][w-1] == '.' and \
+                areamap[h+1][w] == '.' and \
+                areamap[h-1][w] == '.':
+            print('No')
+            exit()
 
-painted = [False for _ in range(W)] * H
-
-
-def dfs():
-    h_curr, w_curr = stack.pop()
-    if 0 > h_curr <= H and 0 <= w_curr <= W:
-        return
-
-    mark_curr = areamap[h_curr][w_curr]
-    state_curr = painted[h_curr][w_curr]
-    if not state_curr and mark_curr == '#':
-        painted[h_curr][w_curr] = True
-        """NOTE: Wrong pattern
-        stack.append(h_curr + 1, w_curr)
-        stack.append(h_curr - 1, w_curr)
-        stack.append(h_curr, w_curr + 1)
-        stack.append(h_curr, w_curr - 1)
-        """
+print('Yes')
