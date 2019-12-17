fb1936608708ebabc808087e0f4fe46940db3f81
ababa893 <flvonlineconverter@gmail.com>
2019-12-17 03:28:52 -1000

    AC

--- contest_095/c.py
+++ contest_095/c.py
@@ -1,9 +1,11 @@
-# WIP (TODO: deal with example 3 )
+# AC
 
 A, B, C, X, Y = map(int, input().split())
 
 cost = 0
 if A + B > 2 * C:
+    n_purchase_surplus = 2 * max(X, Y)
+    cost_surplus = n_purchase_surplus * C
     if X >= Y:
         n_purchase = 2 * Y
         cost = n_purchase * C
@@ -14,6 +16,8 @@ if A + B > 2 * C:
         cost = n_purchase * C
         n_remaining = Y - X
         cost += n_remaining * B
+    # select minimum cost
+    cost = min(cost_surplus, cost)
 else:
     cost = (X * A) + (Y * B)
 print(cost)
