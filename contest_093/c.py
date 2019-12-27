4a00f2592f3632aae4583da5c547e37fdad7709c
ababa893 <flvonlineconverter@gmail.com>
2019-12-27 03:38:32 -1000

    WIP

--- contest_093/c.py
+++ contest_093/c.py
@@ -1,5 +1,9 @@
 A, B, C = map(int, input().split())
 
+if A == B == C:
+    print(0)
+    exit()
+
 combi = [A, B, C]
 max_ABC = max([A, B, C])
 other = [c for c in combi if c != max_ABC]
@@ -11,8 +15,15 @@ n_operation = max_ABC - max_other
 max_other += n_operation
 min_other += n_operation
 
+if max_other == min_other:
+    print(n_operation)
+    exit()
+
 # Operation (2nd method)
-if (max_ABC - min_other) % 2:
+diff = max_ABC - min_other
+if diff % 2 and diff == 1:
+    n_add = (max_ABC - min_other) // 2 + 2
+elif diff % 2:
     n_add = (max_ABC - min_other) // 2 + 1
 else:
     n_add = (max_ABC - min_other) // 2
