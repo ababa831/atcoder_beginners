68ac5ef892207a9e1fad05dab1cc2e46baeea977
ababa893 <flvonlineconverter@gmail.com>
2020-01-05 07:12:53 -1000

    WIP

---
+++ contest_091/c.py
@@ -0,0 +1,7 @@
+# WIP
+import sys
+sys_input = sys.stdin.readline
+
+N = int(sys_input())
+red_points = [tuple(map(int, sys_input.split())) for p in range(N)]
+blue_points = [tuple(map(int, sys_input.split())) for p in range(N)]
