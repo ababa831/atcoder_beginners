859bc2d64e19a09faf7268d17bbf903c0fc0698b
ababa893 <flvonlineconverter@gmail.com>
2020-01-07 10:24:08 -1000

    WIP #2

--- contest_091/c.py
+++ contest_091/c.py
@@ -8,18 +8,5 @@ red_points = sorted(red_points, key=lambda x: (x[0]**2 + x[1]**2)**0.5)
 blue_points = [tuple(map(int, sys_input().split())) for p in range(N)]
 blue_points = sorted(blue_points, key=lambda x: (x[0]**2 + x[1]**2)**0.5)
 
-red_selected = [False] * N
-blue_selected = [False] * N
-
-n_pair = 0
-for blue_point in blue_points:
-    for i, red_point in enumerate(red_points):
-        if red_selected[i]:
-            continue
-        if red_point[0] < blue_point[0] and red_point[1] < blue_point[1]:
-            n_pair += 1
-            red_selected[i] = True
-            blue_selected[i] = True
-            break
-
-print(n_pair)
+# Enumerate all combinations (with duplications)
+# -> How to select non-duplicated pairs? 
