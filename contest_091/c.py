2c6756a502e8f6261595a8196f21944d7ece9f09
ababa893 <flvonlineconverter@gmail.com>
2020-01-12 02:09:57 +0900

    WIP

--- contest_091/c.py
+++ contest_091/c.py
@@ -9,4 +9,10 @@ blue_points = [tuple(map(int, sys_input().split())) for p in range(N)]
 blue_points = sorted(blue_points, key=lambda x: (x[0]**2 + x[1]**2)**0.5)
 
 # Enumerate all combinations (with duplications)
-# -> How to select non-duplicated pairs? 
+# -> How to select non-duplicated pairs?
+canditates = [[None] for _ in range(N)]
+for red in red_points:
+    for i, blue in enumerate(blue_points):
+        if red[0] < blue[0] and red[1] < blue[1]:
+            canditates[i].append(red)
+# WIP
