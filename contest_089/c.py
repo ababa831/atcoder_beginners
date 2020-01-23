e4f95bd8f5d3fc8f611d4f6d8cb98eaeebdb573b
ababa893 <flvonlineconverter@gmail.com>
2020-01-23 11:49:40 +0900

    WIP

--- contest_089/c.py
+++ contest_089/c.py
@@ -6,6 +6,17 @@ SSS = [input() for _ in range(N)]
 
 # Filter1
 filter_1 = {'M', 'A', 'R', 'C', 'H'}
-canditates = [S for S in SSS if S[0] in filter_1]
+canditates = set([S for S in SSS if S[0] in filter_1])
 cand_inits = [c[0] for c in canditates]
 init_counts = {init_c: cand_inits.count(init_c) for init_c in filter_1}
+
+# TODO: Deal with duplication
+# e.g. {C, M1, M2, R1, R2, A}
+# patterns: {[C, M, R], [C, M, A], [C, R, A], [M, R, A]}
+#    if M and R in pattern:
+#       combi1 = pattern * n_Mgroup * n_Rgroup
+#    if M in pattern:
+#       combi2 = pattern * n_Mgroup
+#    if R in pattern:
+#       combi3 = pattern * n_Rgroup
+# total_combinations = combi1 + combi2 + combi3 - (N-n_Mgroup-n_Rgroup)C3
