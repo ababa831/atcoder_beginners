924192f6d7c9f9a56b2661aeeaa6f03c07de33f7
ababa893 <flvonlineconverter@gmail.com>
2020-01-24 01:52:45 -1000

    AC #3

--- contest_089/c.py
+++ contest_089/c.py
@@ -1,4 +1,4 @@
-# WIP
+# AC
 from itertools import combinations
 
 N = int(input())
@@ -6,17 +6,17 @@ SSS = [input() for _ in range(N)]
 
 # Filter1
 filter_1 = {'M', 'A', 'R', 'C', 'H'}
-canditates = set([S for S in SSS if S[0] in filter_1])
+canditates = [S for S in SSS if S[0] in filter_1]
 cand_inits = [c[0] for c in canditates]
+cand_inits_set = set(cand_inits)
 init_counts = {init_c: cand_inits.count(init_c) for init_c in filter_1}
 
-# TODO: Deal with duplication
-# e.g. {C, M1, M2, R1, R2, A}
-# patterns: {[C, M, R], [C, M, A], [C, R, A], [M, R, A]}
-#    if M and R in pattern:
-#       combi1 = pattern * n_Mgroup * n_Rgroup
-#    if M in pattern:
-#       combi2 = pattern * n_Mgroup
-#    if R in pattern:
-#       combi3 = pattern * n_Rgroup
-# total_combinations = combi1 + combi2 + combi3 - (N-n_Mgroup-n_Rgroup)C3
+n_combi = 0
+for init_combi in combinations(cand_inits_set, 3):
+    tmp_combi = 1
+    for init_c in init_combi:
+        tmp_combi *= init_counts[init_c]
+
+    n_combi += tmp_combi
+
+print(n_combi)
