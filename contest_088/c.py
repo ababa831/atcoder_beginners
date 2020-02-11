fcc5a01c1be85d580a0e238929b4f2096727d76b
ababa893 <flvonlineconverter@gmail.com>
2020-02-11 10:03:33 -1000

    Minor fix

--- contest_088/c.py
+++ contest_088/c.py
@@ -1,4 +1,4 @@
-# WIP (WA: in18.txt, in19.txt) 
+# WIP (WA: in18.txt, in19.txt)
 C11, C12, C13 = map(int, input().split())
 C21, C22, C23 = map(int, input().split())
 C31, C32, C33 = map(int, input().split())
