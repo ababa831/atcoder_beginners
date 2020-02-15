67a335fcd447e57a5525f5a6c2391610632e4937
ababa893 <flvonlineconverter@gmail.com>
2020-02-15 09:05:26 -1000

    Delete codes no longer used

--- contest_090/c.py
+++ contest_090/c.py
@@ -11,36 +11,3 @@ elif N > 2 and M > 2:
     print((N - 2) * (M - 2))
 else:
     print(0)
-
-"""
-# An experiment
-n_count_map = [[0 for _ in range(M)] for _ in range(N)]
-
-
-def update(x, y):
-    # Count
-    n_count_map[y][x] += 1
-    if y + 1 < N:
-        n_count_map[y + 1][x] += 1
-    if y - 1 >= 0:
-        n_count_map[y - 1][x] += 1
-    if x + 1 < M:
-        n_count_map[y][x + 1] += 1
-    if x - 1 >= 0:
-        n_count_map[y][x - 1] += 1
-    if y + 1 < N and x + 1 < M:
-        n_count_map[y + 1][x + 1] += 1
-    if y - 1 >= 0 and x - 1 >= 0:
-        n_count_map[y - 1][x - 1] += 1
-    if y + 1 < N and x - 1 >= 0:
-        n_count_map[y + 1][x - 1] += 1
-    if y - 1 >= 0 and x + 1 < M:
-        n_count_map[y - 1][x + 1] += 1
-
-
-for n in range(N):
-    for m in range(M):
-        update(m, n)
-
-print(n_count_map)
-"""
